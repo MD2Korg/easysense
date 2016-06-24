b19e1e50cfa7006ff4577b3e8b9e02278791b472
Timothy Hnat <twhnat@memphis.edu>
2016-06-24 18:21:58 +0000

    Missing script added

--- radar/easysense.sh
+++ radar/easysense.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-/home/root/easysense/radar/collectData $1 $2
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/:/usr/local/lib
+cd /home/root/easysense/radar/
+./collectData $1 $2 > /home/root/easysense.log 2>/home/root/easysense_error.log
